66907d3b3249b10497f6f81eaeff3af2a6b8b200
Kaiobr13 <114837086+Kaiobr13@users.noreply.github.com>
2024-09-18 11:20:32 +0100

    Create DB Folder

---
+++ Database/Create Tables.sql
@@ -0,0 +1,20 @@
+create table users(
+                    user_id int not null auto_increment primary key,                         -- Primary key, auto incrementing
+                    user_name varchar(120),                                                  -- User name, variable length
+                    user_email varchar(120) unique not null,                                 -- User email, it is unique and not null
+                    user_logo longblob,                                                      -- User logo, binary data
+                    user_password varchar(60) not null                                       -- User password, fixed length
+);
+
+create table product(
+                    product_id int not null auto_increment primary key,                       -- Primary key, auto incrementing
+                    product_name text,                                                        -- Product name, variable lenght
+                    product_type_id int,                                                      -- Product type
+                    product_price decimal(13,2) not null,                                     -- Product value, fixed lenght
+                    product_quantity int not null                                             -- Product quantity
+); 
+
+create table productType(
+                        type_id int not null auto_increment primary key,                      -- Primary key, auto incrementing
+                        type_name varchar(60) not null                                        -- Product type name, variable length
+);                     
