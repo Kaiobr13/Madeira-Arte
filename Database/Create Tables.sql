6ed2240e32c142070fbebd743b9b5bdb7b53a2ed
Kaiobr13 <kaiobs920@gmail.com>
2024-09-20 11:08:47 +0100

    Adjustments within Create Tables

--- Database/Create Tables.sql
+++ Database/Create Tables.sql
@@ -2,15 +2,16 @@ create table users(
                     user_id int not null auto_increment primary key,                         -- Primary key, auto incrementing
                     user_name varchar(120),                                                  -- User name, variable length
                     user_email varchar(120) unique not null,                                 -- User email, it is unique and not null
-                    user_logo longblob,                                                      -- User logo, binary data
+                    user_place varchar(60),                                                  -- User Address
                     user_password varchar(60) not null                                       -- User password, fixed length
 );
 
 create table product(
                     product_id int not null auto_increment primary key,                       -- Primary key, auto incrementing
                     product_name text,                                                        -- Product name, variable lenght
-                    product_type_id int,                                                      -- Product type
+                    product_type_id int,                                                      -- Product type, Foreign Key
                     product_price decimal(13,2) not null,                                     -- Product value, fixed lenght
+                    product_logo longblob,                                                    -- Product logo, binary data
                     product_quantity int not null                                             -- Product quantity
 ); 
 
